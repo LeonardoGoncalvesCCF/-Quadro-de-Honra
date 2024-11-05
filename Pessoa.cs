c15c9cb9e247c1a39b8b6529e2c27b26812c46a7
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2024-11-04 21:40:30 -0300

    Primeiro Commit

---
+++ Pessoa.cs
@@ -0,0 +1,8 @@
+
+public class pessoas
+{
+    public string 
+    public string 
+    public string 
+    public string 
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string[] pessoas = new string[5]; 
+
+for(int i = 0; i < 5; i++)
+{
+   System.Console.WriteLine("Digite Nomes: ");
+   var NomeTemp = Console.ReadLine();
+   pessoas[i] = NomeTemp;
+}
+System.Console.WriteLine("------------");
+foreach(string nome in pessoas)
+{
+   System.Console.WriteLine(nome);
+}
+
+ 
